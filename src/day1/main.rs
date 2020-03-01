2807989e0f2f492699fceedb66ade4fcffc625fa
Simon Kocurek <simon.kocurek@gmail.com>
2020-03-01 11:51:04 +0100

    Move files

---
+++ src/day1/main.rs
@@ -0,0 +1,27 @@
+use std::io::stdin;
+
+fn main() {
+    let result: i32 = read_input()
+        .into_iter()
+        .map(|mass| mass / 3 - 2)
+        .sum();
+    println!("{}", result);
+}
+
+fn read_input() -> Vec<i32> {
+    let mut vec = vec![];
+
+    let mut buffer = String::new();
+    let stdin = stdin();
+    while let Ok(_bytes) = stdin.read_line(&mut buffer) {
+        let trimmed = buffer.trim();
+        if trimmed.is_empty() {
+            break
+        }
+
+        vec.push(trimmed.parse().unwrap());
+        buffer.clear();
+    }
+
+    vec
+}
